561f3efd40afd9f36788daf3c5c9bae8cbdd4d8d
Diego Martín del Campo <99349485+dmdcl@users.noreply.github.com>
2023-02-08 18:13:59 -0600

    Add files via upload

---
+++ EVA1/EVA1_1_SUMA/src/eva1_1_suma/EVA1_1_SUMA.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+//Programa que usa JOptionPane para realizar una suma
+package eva1_1_suma;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author diego
+ */
+public class EVA1_1_SUMA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //obtiene la entrada que diga el usuario de los dialogos con JOptionPane
+        String primerNumero = JOptionPane.showInputDialog("Introduzca el primer numero: ");
+        String segundoNumero = JOptionPane.showInputDialog("Introduzca el segundo numero: ");
+                
+    }
+    
+}
+
+
